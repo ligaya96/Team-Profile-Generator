7816caf4a3c647487835b184f15d098173c58a02
ligaya <ligaya96@gmail.com>
2021-02-26 21:15:41 -0800

    first commit, added files

---
+++ index.js
@@ -0,0 +1,18 @@
+// Employee 
+
+// Manager
+//office number
+//getRole()
+
+// Engineer
+//gitHub getGithub()
+//github Username 
+
+// Intern
+// school getSchool()
+// getRole()
+
+// class/ properties
+// name getName()
+//id getID()
+//email getEmail()
